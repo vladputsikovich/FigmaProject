50756bff69c3787b76f28c9fefe6896d2f5bb094
vladputsikovich <vladputsikovich@gmail.com>
2022-02-20 20:28:32 +0300

    Initial Commit

---
+++ Figma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Figma
+//
+//  Created by Владислав Пуцыкович on 20.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
